ddc59a5106042876af42a20c83de0265cc3e2dc1
Wess Cope <me@wess.io>
2024-08-13 11:01:57 -0400

    updates for release

--- mix.exs
+++ mix.exs
@@ -2,17 +2,27 @@ defmodule Tasks.MixProject do
   use Mix.Project
 
   @version "0.0.1"
-  @description "Mix tasks"
+  @description "Some nice additional tasks for Mix"
+  @repo "https://github.com/devpipe/mix_tasks"
 
   def project do
     [
       app: :tasks,
       version: @version,
       description: @description,
+      package: package(),
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
+  end
+
+  defp package do
+    %{
+      licenses: ["MIT"],
+      maintainers: ["Wess Cope"],
+      links: %{"Github" => @repo}
+    }
   end
 
   # Run "mix help compile.app" to learn about applications.
